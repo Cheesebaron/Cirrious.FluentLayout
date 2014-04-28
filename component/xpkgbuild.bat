d3ee162d79159d90a2b57db52f4ba7074ee4f90c
Tomasz Cielecki <tomasz@ostebaronen.dk>
2014-04-29 01:33:15 +0200

    Update xpkgbuild.bat

--- component/xpkgbuild.bat
+++ component/xpkgbuild.bat
@@ -1,6 +1,6 @@
 :: Grab xpkg from http://components.xamarin.com/submit/xpkg and put contents of the zip into xpkg
 
-xpkg\xamarin-component.exe create-manually FluentLayout-1.0.xam ^
+xpkg\xamarin-component.exe create-manually fluentlayout-1.0.xam ^
 	--name="FluentLayout" ^
 	--summary="FluentLayout provides a fluent API for constraint-based layouts in Xamarin.iOS." ^
 	--website="https://github.com/slodge/Cirrious.FluentLayout" ^
@@ -10,4 +10,5 @@ xpkg\xamarin-component.exe create-manually FluentLayout-1.0.xam ^
 	--icon="icons/fluentlayout_128x128.png" ^
 	--icon="icons/fluentlayout_512x512.png" ^
 	--library="ios":"../Cirrious.FluentLayout/bin/iPhone/Debug/iPhone/Cirrious.FluentLayout.dll" ^
-	--publisher "Cheesebaron"
+	--publisher "Cheesebaron" ^
+	--sample="Sample app":"../QuickLayout.sln"
